095c1cc4b1818fe696a36a56647eb83a6586092d
omaresmaeel <omaresmael590@gmail.com>
2025-01-11 18:11:35 +0200

    use parallel extension took 30 sec 87.5% improvement

--- src/calculateMeasurements.php
+++ src/calculateMeasurements.php
@@ -1,46 +1,104 @@
 <?php
 
-$file = fopen('./measurements.txt', 'r');
-$stations = [];
-$start = hrtime()[0];
+use parallel\{Runtime, Channel};
 
-$chunkSize = 1048576; // 1MB
-$chunkNumber = 0;
+$startTime = hrtime()[0];
+$file = './measurements.txt';
+$numThreads = 16;
+$chunkSize = 1000000;
 
-while (!feof($file)) {
-    $data = fread($file, $chunkSize);
-    $lines = explode("\n", $data);
-    foreach ($lines as $line) {
-        $pos = strpos($line, ';');
-        $station = substr($line, 0, $pos);
-        $temperature = (int) substr($line, $pos+1, -1);
-        if (!isset($stations[$station])) {
-            $stations[$station]['min'] = $temperature;
-            $stations[$station]['max'] = $temperature;
-            $stations[$station]['count'] = 1;
-            $stations[$station]['sum'] = $temperature;
-            continue;
+$fileSize = filesize($file);
+$features = [];
+
+$chunkPerThread = ceil($fileSize / $numThreads);
+
+for ($i = 0; $i < $numThreads; $i++) {
+    $runtime = new Runtime();
+
+    $start = $i * $chunkPerThread;
+    $end = ($i + 1) * $chunkPerThread;
+
+    if ($i === $numThreads - 1) {
+        $end = $fileSize;
+    }
+
+    $features[] = $runtime->run(function($file, $start, $end, $chunkSize) {
+        $stations = [];
+        $fileHandle = fopen($file, 'r');
+
+        fseek($fileHandle, $start);
+
+        $buffer = '';
+        while (ftell($fileHandle) < $end && !feof($fileHandle)) {
+
+            $data = fread($fileHandle, $chunkSize);
+            $buffer .= $data;
+
+            $lines = explode("\n", $buffer);
+
+            for ($i = 0; $i < count($lines) - 1; $i++) {
+                $line = $lines[$i];
+                $pos = strpos($line, ';');
+
+                $station = substr($line, 0, $pos);
+                $temperature = (int) substr($line, $pos + 1);
+
+                if (!isset($stations[$station])) {
+                    $stations[$station] = [
+                        'min' => $temperature,
+                        'max' => $temperature,
+                        'count' => 1,
+                        'sum' => $temperature
+                    ];
+                } else {
+                    $stations[$station]['count']++;
+                    $stations[$station]['sum'] += $temperature;
+                    if ($temperature < $stations[$station]['min']) {
+                        $stations[$station]['min'] = $temperature;
+                    }
+                    if ($temperature > $stations[$station]['max']) {
+                        $stations[$station]['max'] = $temperature;
+                    }
+                }
+            }
+
+
+            $buffer = $lines[count($lines) - 1];
         }
-        $stations[$station]['count']++;
-        $stations[$station]['sum'] += $temperature;
-        if ($temperature < $stations[$station]['min']) {
-            $stations[$station]['min'] = $temperature;
-            continue;
+
+        fclose($fileHandle);
+
+        return $stations;
+    }, [$file, $start, $end, $chunkSize]);
+}
+
+$results = [];
+for ($i = 0; $i < $numThreads; $i++) {
+    $results[] = $features[$i]->value();
+}
+
+$finalStations = [];
+foreach ($results as $threadResult) {
+    foreach ($threadResult as $station => $data) {
+        if (!isset($finalStations[$station])) {
+            $finalStations[$station] = $data;
+        } else {
+            $finalStations[$station]['count'] += $data['count'];
+            $finalStations[$station]['sum'] += $data['sum'];
+            if ($data['min'] < $finalStations[$station]['min']) {
+                $finalStations[$station]['min'] = $data['min'];
+            }
+            if ($data['max'] > $finalStations[$station]['max']) {
+                $finalStations[$station]['max'] = $data['max'];
+            }
         }
-        $stations[$station]['max'] = $temperature;
     }
 }
-fclose($file);
-ksort($stations);
-foreach ($stations as $station => $data) {
-    $mean = $data['sum'] / $data['count'];
-    echo "{\n";
-    echo "Station: $station\n";
-    echo "Min: {$data['min']}\n";
-    echo "Max: {$data['max']}\n";
-    echo "Mean: $mean\n";
-    echo "}\n";
-    echo "\n";
+
+foreach ($finalStations as $station => $data) {
+    $average = $data['sum'] / $data['count'];
+    echo "$station/{$data['min']}/$average/{$data['max']}\n";
 }
-$end = $start = hrtime()[0] - $start;
+
+$end = hrtime()[0] - $startTime;
 echo "Time in sec: $end\n";
